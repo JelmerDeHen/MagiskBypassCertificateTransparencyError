b728e4223ec6a829f1777760441b8fd8b8b2f12c
J <18084450+JelmerDeHen@users.noreply.github.com>
2022-10-23 15:19:20 +0200

    Did not need SPKI fingerprint

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,65 +1,22 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-# static openssl
-openssl="${MODDIR}/openssl"
-chmod +x "${openssl}"
+printf 'chrome --ignore-certificate-errors-spki-list=1' "${SPKI}" > "${MODDIR}/chrome-command-line"
 
-# This script will be executed in post-fs-data mode
-# More info in the main Magisk thread
-
-# Consumes cert in DER format and prints SPKI fingerprint
-der2spki () {
-  $openssl x509 -inform der | \
-    $openssl x509 -pubkey -noout | \
-    $openssl pkey -pubin -outform der | \
-    $openssl dgst -sha256 -binary | \
-    $openssl enc -base64
-}
-
-# Create SPKI fingerprints for all user installed certificates
-getUserCertSPKIs() {
-  find /data/misc/user/0/cacerts-added -type f | while read -r; do
-    FINGERPRINT="$(der2spki < "${REPLY}")"
-    if [ "${FINGERPRINT}x" = "x" ]; then
-      continue
-    fi
-    printf '%s,' "$(der2spki < "${REPLY}")"
-  done
-}
-
-# Creates chrome-command-file with chrome flags
-createFlagFile() {
-  SPKI="$(getUserCertSPKIs)"
-  SPKI="${SPKI%,}"
-
-  if [ "${SPKI}x" = "x" ]; then
-    return 1
-  fi
-  
-  printf 'chrome --ignore-certificate-errors-spki-list=%s\n' "${SPKI}" > "${MODDIR}/chrome-command-line"
-}
-
-# Duplicates the created flag file to /data
-copyFlagFileToData() {
-  while read -r; do
-    cp "${MODDIR}/chrome-command-line" "${REPLY}"
-    chmod 555 "${REPLY}"
-  done < "${MODDIR}/flagfiles.txt"
-}
+while read -r; do
+  cp "${MODDIR}/chrome-command-line" "${REPLY}"
+  chmod 555 "${REPLY}"
+done < <<EOF
+/data/local/chrome-command-line
+/data/local/android-webview-command-line
+/data/local/webview-command-line
+/data/local/content-shell-command-line
+/data/local/tmp/chrome-command-line
+/data/local/tmp/android-webview-command-line
+/data/local/tmp/webview-command-line
+/data/local/tmp/content-shell-command-line
+EOF
 
 # Causes shouldUseDebugCommandLine() to return true in Chrome while using user build type rom
-fixUserDebug() {
-  settings put global adb_enabled 1
-  settings put global debug_app com.android.chrome
-}
-
-main() {
-  createFlagFile || abort "Could not create flag file, did you install any user certificates?"
-  copyFlagFileToData
-  fixUserDebug
-}
-
-main
+settings put global adb_enabled 1
+settings put global debug_app com.android.chrome
